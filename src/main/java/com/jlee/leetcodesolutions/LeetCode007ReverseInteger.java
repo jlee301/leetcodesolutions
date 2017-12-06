1c5e86966eb200813910df3c696f0c3bc68c6028
John Lee <johnlee@Johns-MBP.hsd1.ca.comcast.net>
2017-12-05 22:09:47 -0800

    Added comments for 007 - Reverse Integer.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode007ReverseInteger.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode007ReverseInteger.java
@@ -18,18 +18,19 @@ class LeetCode007ReverseInteger {
 	 * https://leetcode.com/problems/reverse-integer/description/
 	 */
     public int reverse(int x) {
+    		// Use long here to compensate for 32-bit overflow
 		long result = 0;
 		while (x != 0) {
 			int remainder = x % 10;
-			//System.out.println("Remainder: " + remainder);
+			// System.out.println("Remainder: " + remainder);
 			result = (result * 10) + remainder;
-			//System.out.println("Result: " + result);
+			// System.out.println("Result: " + result);
 			x = x / 10;
 			//System.out.println("x: " + x);
 		}
 		if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
-    			//Integer.MAX_VALUE = 2147483647
-    			//Integer.MIN_VALUE = -2147483648
+    			// Integer.MAX_VALUE = 2147483647
+    			// Integer.MIN_VALUE = -2147483648
 			return 0;
 		}
 		else {

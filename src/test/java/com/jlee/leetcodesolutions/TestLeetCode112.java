144dd0bcfed5202431cfb0b09b34933b8ca5b663
John Lee <johnlee@Johns-MBP.hsd1.ca.comcast.net>
2017-12-12 19:16:44 -0800

    Adding test cases for 112.

--- src/test/java/com/jlee/leetcodesolutions/TestLeetCode112.java
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode112.java
@@ -89,6 +89,31 @@ public class TestLeetCode112 {
   }
 
   @Test
+  public void testProblemExample() {
+    TreeNode root = new TreeNode(5);
+    root.left = new TreeNode(4);
+    root.left.left = new TreeNode(11);
+    root.left.left.left = new TreeNode(7);
+    root.left.left.right = new TreeNode(2);
+    root.right = new TreeNode(8);
+    root.right.left = new TreeNode(13);
+    root.right.right = new TreeNode(4);
+    root.right.right.right = new TreeNode(1);
+
+    LeetCode112 solution = new LeetCode112();
+    boolean result = solution.hasPathSum(root, 22);
+    Assert.assertTrue(result);
+    result = solution.hasPathSum(root, 27);
+    Assert.assertTrue(result);
+    result = solution.hasPathSum(root, 26);
+    Assert.assertTrue(result);
+    result = solution.hasPathSum(root, 18);
+    Assert.assertTrue(result);
+    result = solution.hasPathSum(root, 17);
+    Assert.assertFalse(result);
+  }
+  
+  @Test
   public void testNull() {
     LeetCode112 solution = new LeetCode112();
     boolean result = solution.hasPathSum(null, 0);

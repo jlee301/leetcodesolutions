dae4632dac56a51f76faaf9df4dc4cf6f5c08f0b
John Lee <johnlee@Johns-MBP.hsd1.ca.comcast.net>
2017-12-19 01:16:50 -0800

    Adding solution for 242 using a Hash array.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode242.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode242.java
@@ -30,4 +30,29 @@ public class LeetCode242 {
    
    return Arrays.equals(a, b);
   }
+  
+  public boolean isAnagramUsingHash(String s, String t) {
+    if(s == null || t == null || s.length() != t.length())
+      return false;
+    
+    // Hash for character set a-z
+    // hash[0] = a
+    // hash[25] = z
+    int[] hash = new int[26];
+    for(int i = 0; i < s.length(); i++) {
+      // s.charAt(i) = 'a'
+      // s.charAt(i) - 'a' = 0;
+      //
+      // s.charAt(i) = 'z'
+      // s.charAt(i) - 'a' = 25
+      hash[s.charAt(i) - 'a']++;
+      hash[t.charAt(i) - 'a']--;
+    }
+    
+    for(int i = 0; i < 26; i++) {
+      if(hash[i] != 0)
+        return false;
+    }
+    return true;
+  }
 }
--- src/test/java/com/jlee/leetcodesolutions/TestLeetCode242.java
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode242.java
@@ -49,4 +49,49 @@ public class TestLeetCode242 {
     LeetCode242 solution = new LeetCode242();
     Assert.assertFalse(solution.isAnagram(s, t));
   }
+
+  @Test
+  public void testProblemCase1UsingHash() {
+    String s = "anagram";
+    String t = "nagaram";
+    
+    LeetCode242 solution = new LeetCode242();
+    Assert.assertTrue(solution.isAnagramUsingHash(s, t));
+  }
+
+  @Test
+  public void testProblemCase2UsingHash() {
+    String s = "rat";
+    String t = "car";
+    
+    LeetCode242 solution = new LeetCode242();
+    Assert.assertFalse(solution.isAnagramUsingHash(s, t));
+  }
+
+  @Test
+  public void testMixSizeUsingHash() {
+    String s = "rat";
+    String t = "ratt";
+    
+    LeetCode242 solution = new LeetCode242();
+    Assert.assertFalse(solution.isAnagramUsingHash(s, t));
+  }
+
+  @Test
+  public void testEmptyUsingHash() {
+    String s = "";
+    String t = "";
+    
+    LeetCode242 solution = new LeetCode242();
+    Assert.assertTrue(solution.isAnagramUsingHash(s, t));
+  }
+
+  @Test
+  public void testNullUsingHash() {
+    String s = null;
+    String t = null;
+    
+    LeetCode242 solution = new LeetCode242();
+    Assert.assertFalse(solution.isAnagramUsingHash(s, t));
+  }
 }

19c07731ea78628c16cd35f524863e42c3727b85
John Lee <johnlee@Johns-MBP.hsd1.ca.comcast.net>
2017-12-23 23:36:30 -0800

    Adding solution for 412 - Fizz Buzz.

---
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode412.java
@@ -0,0 +1,34 @@
+package com.jlee.leetcodesolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode412 {
+  /*
+   * Write a program that outputs the string representation of numbers from 1 to
+   * n.
+   * 
+   * But for multiples of three it should output “Fizz” instead of the number and
+   * for the multiples of five output “Buzz”. For numbers which are multiples of
+   * both three and five output “FizzBuzz”.
+   * 
+   * https://leetcode.com/problems/fizz-buzz/description/
+   */
+  public List<String> fizzBuzz(int n) {
+    List<String> result = new ArrayList<String>();
+    if(n <= 0)
+      return result;
+    
+    for(int i = 1; i <= n ; i++) {
+      if(i % 3 == 0 && i % 5 == 0)
+        result.add("FizzBuzz");
+      else if (i % 3 == 0)
+        result.add("Fizz");
+      else if (i % 5 == 0)
+        result.add("Buzz");
+      else
+        result.add(Integer.toString(i));
+    }
+    return result;
+  }
+}
---
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode412.java
@@ -0,0 +1,102 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.LeetCode412;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLeetCode412 {
+  @Test
+  public void test1() {
+    int n = 1;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(1, result.size());
+    Assert.assertEquals("1", result.get(0));
+  }
+
+  @Test
+  public void test2() {
+    int n = 2;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(2, result.size());
+    Assert.assertEquals("1", result.get(0));
+    Assert.assertEquals("2", result.get(1));
+  }
+
+  @Test
+  public void test3() {
+    int n = 3;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(3, result.size());
+    Assert.assertEquals("1", result.get(0));
+    Assert.assertEquals("2", result.get(1));
+    Assert.assertEquals("Fizz", result.get(2));
+  }
+
+  @Test
+  public void test4() {
+    int n = 4;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(4, result.size());
+    Assert.assertEquals("1", result.get(0));
+    Assert.assertEquals("2", result.get(1));
+    Assert.assertEquals("Fizz", result.get(2));
+    Assert.assertEquals("4", result.get(3));
+  }
+
+  @Test
+  public void test5() {
+    int n = 5;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(5, result.size());
+    Assert.assertEquals("1", result.get(0));
+    Assert.assertEquals("2", result.get(1));
+    Assert.assertEquals("Fizz", result.get(2));
+    Assert.assertEquals("4", result.get(3));
+    Assert.assertEquals("Buzz", result.get(4));
+  }
+
+  @Test
+  public void test15() {
+    int n = 15;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(15, result.size());
+    Assert.assertEquals("1", result.get(0));
+    Assert.assertEquals("2", result.get(1));
+    Assert.assertEquals("Fizz", result.get(2));
+    Assert.assertEquals("4", result.get(3));
+    Assert.assertEquals("Buzz", result.get(4));
+    Assert.assertEquals("Fizz", result.get(5));
+    Assert.assertEquals("7", result.get(6));
+    Assert.assertEquals("8", result.get(7));
+    Assert.assertEquals("Fizz", result.get(8));
+    Assert.assertEquals("Buzz", result.get(9));
+    Assert.assertEquals("11", result.get(10));
+    Assert.assertEquals("Fizz", result.get(11));
+    Assert.assertEquals("13", result.get(12));
+    Assert.assertEquals("14", result.get(13));
+    Assert.assertEquals("FizzBuzz", result.get(14));
+  }
+
+  @Test
+  public void test0() {
+    int n = 0;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testNeg1() {
+    int n = -1;
+    LeetCode412 solution = new LeetCode412();
+    List<String> result = solution.fizzBuzz(n);
+    Assert.assertEquals(0, result.size());
+  }
+}

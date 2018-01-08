c47e3072439cfc9b064d0e0dd186a4d319bccef8
John Lee <john.m.lee@gmail.com>
2018-01-07 23:17:24 -0800

    Adding solution for 562 - Binary Tree Tilt.

---
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode562.java
@@ -0,0 +1,61 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.TreeNode;
+
+public class LeetCode562 {
+  /*
+   * Given a binary tree, return the tilt of the whole tree.
+   * 
+   * The tilt of a tree node is defined as the absolute difference between the sum
+   * of all left subtree node values and the sum of all right subtree node values.
+   * Null node has tilt 0.
+   * 
+   * The tilt of the whole tree is defined as the sum of all nodes' tilt.
+   * 
+   * Example:
+   * Input:
+   *   1
+   *  / \
+   * 2   3
+   * 
+   * Output: 1
+   * 
+   * Explanation:
+   * Tilt of node 2 : 0
+   * Tilt of node 3 : 0
+   * Tilt of node 1 : |2-3| = 1
+   * Tilt of binary tree : 0 + 0 + 1 = 1
+   * 
+   * Note:
+   * 1. The sum of node values in any subtree won't exceed the range of 32-bit
+   * integer.
+   * 2.All the tilt values won't exceed the range of 32-bit integer.
+   * 
+   * https://leetcode.com/problems/binary-tree-tilt/description/
+   */
+  private int tilt = 0;
+  public int findTilt(TreeNode root) {
+    if(root == null)
+      return tilt;
+    
+    findTiltAt(root);
+    return tilt;
+  }
+  
+  private void findTiltAt(TreeNode root) {
+    if(root.left != null && root.right != null) {
+      tilt += Math.abs(root.left.val - root.right.val);
+      findTiltAt(root.left);
+      findTiltAt(root.right);
+      
+    } else if(root.left != null && root.right == null) {
+      tilt += Math.abs(root.left.val - 0);
+      findTiltAt(root.left);
+      
+    } else if(root.left == null && root.right != null) {
+      tilt += Math.abs(0 - root.right.val);
+      findTiltAt(root.right);
+      
+    }
+  }
+}
---
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode562.java
@@ -0,0 +1,82 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.LeetCode562;
+import com.jlee.leetcodesolutions.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLeetCode562 {
+  @Test
+  public void testJustRoot() {
+    TreeNode root = new TreeNode(1);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(0, solution.findTilt(root));
+  }
+
+  @Test
+  public void testRootLeft() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(2, solution.findTilt(root));
+  }
+
+  @Test
+  public void testRootRight() {
+    TreeNode root = new TreeNode(1);
+    root.right = new TreeNode(3);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(3, solution.findTilt(root));
+  }
+
+  @Test
+  public void testRootLeftRight() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(1, solution.findTilt(root));
+  }
+
+  @Test
+  public void testThreeNodeLeft() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right = new TreeNode(3);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(2, solution.findTilt(root));
+  }
+
+  @Test
+  public void testThreeNodeRight() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(7);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(2, solution.findTilt(root));
+  }
+
+  @Test
+  public void testThreeNodeLeftRight() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right = new TreeNode(3);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(7);
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(3, solution.findTilt(root));
+  }
+
+  @Test
+  public void testNull() {
+    TreeNode root = null;
+    LeetCode562 solution = new LeetCode562();
+    Assert.assertEquals(0, solution.findTilt(root));
+  }
+}

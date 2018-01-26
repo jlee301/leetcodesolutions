a2f75f4e927e399d2b4112f35931cd1068fb32d4
John Lee <john.m.lee@gmail.com>
2018-01-25 21:44:22 -0800

    Adding solution for 017 - Letter Combinations of a Phone Number.

---
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode017.java
@@ -0,0 +1,43 @@
+package com.jlee.leetcodesolutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeetCode017 {
+  /*
+   * Given a digit string, return all possible letter combinations that the number
+   * could represent.
+   * 
+   * Input: Digit string "23"
+   * 
+   * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+   * 
+   * Note: Although the above answer is in lexicographical order, your answer
+   * could be in any order you want.
+   * 
+   * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+   */
+  public List<String> letterCombinations(String digits){
+    List<String> answer = new ArrayList<String>();
+    if(digits == null || digits.length() == 0 || digits.indexOf("1") != -1 || digits.indexOf("0") != -1)
+      return answer;
+    
+    String[][] map = { {}, {}, { "a", "b", "c" }, { "d", "e", "f" }, { "g", "h", "i" }, { "j", "k", "l" },
+        { "m", "n", "o" }, { "p", "q", "r", "s" }, { "t", "u", "v" }, { "w", "x", "y", "z" } };
+    
+    String[] result = map[Character.getNumericValue(digits.charAt(0))];
+    for(int i = 1; i < digits.length(); i++) {
+      String[] temp = map[Character.getNumericValue(digits.charAt(i))];
+      String[] newResult = new String[result.length * temp.length];
+      int count = 0;
+      for(int j = 0; j < result.length; j++) {
+        for(int k = 0; k < temp.length; k++)
+          newResult[count++] = result[j] + temp[k];
+      }
+      result = newResult;
+    }
+    answer = Arrays.asList(result);
+    return answer;
+  }
+}
---
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode017.java
@@ -0,0 +1,68 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.LeetCode017;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLeetCode017 {
+  @Test
+  public void testProblemCase() {
+    String digits = "23";
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(9, result.size());
+    Assert.assertEquals("ad", result.get(0));
+    Assert.assertEquals("ae", result.get(1));
+    Assert.assertEquals("af", result.get(2));
+    Assert.assertEquals("bd", result.get(3));
+    Assert.assertEquals("be", result.get(4));
+    Assert.assertEquals("bf", result.get(5));
+    Assert.assertEquals("cd", result.get(6));
+    Assert.assertEquals("ce", result.get(7));
+    Assert.assertEquals("cf", result.get(8));
+  }
+
+  @Test
+  public void testSingleDigit() {
+    String digits = "2";
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(3, result.size());
+    Assert.assertEquals("a", result.get(0));
+    Assert.assertEquals("b", result.get(1));
+    Assert.assertEquals("c", result.get(2));
+  }
+
+  @Test
+  public void testContainsOne() {
+    String digits = "21";
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testContainsZero() {
+    String digits = "20";
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testEmpty() {
+    String digits = "";
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testNull() {
+    String digits = null;
+    LeetCode017 solution = new LeetCode017();
+    List<String> result = solution.letterCombinations(digits);
+    Assert.assertEquals(0, result.size());
+  }
+}

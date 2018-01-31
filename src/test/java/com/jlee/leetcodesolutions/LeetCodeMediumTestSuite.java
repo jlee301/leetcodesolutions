14c0863c3ad90347ff6dbb17afcd683d97d611e6
John Lee <john.m.lee@gmail.com>
2018-01-31 00:52:16 -0800

    Adding Easy and Medium test suites.

---
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeMediumTestSuite.java
@@ -0,0 +1,39 @@
+package com.jlee.leetcodesolutions;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ 
+  TestLeetCode002.class, 
+  TestLeetCode003.class, 
+  TestLeetCode005.class, 
+  TestLeetCode006.class, 
+  TestLeetCode008.class, 
+  TestLeetCode011.class, 
+  TestLeetCode012.class, 
+  TestLeetCode015.class, 
+  TestLeetCode016.class, 
+  TestLeetCode017.class, 
+  TestLeetCode018.class, 
+  TestLeetCode019.class, 
+  TestLeetCode022.class, 
+  TestLeetCode024.class, 
+  TestLeetCode029.class, 
+  TestLeetCode031.class, 
+  TestLeetCode033.class, 
+  TestLeetCode034.class, 
+  TestLeetCode036.class, 
+  TestLeetCode039.class, 
+  TestLeetCode040.class, 
+  TestLeetCode043.class, 
+  TestLeetCode046.class, 
+  TestLeetCode047.class, 
+  TestLeetCode048.class 
+})
+
+public class LeetCodeMediumTestSuite {
+
+}

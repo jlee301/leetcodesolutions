f1e873ffb70ba1cc13794fad162652b40aea68a3
John Lee <john.m.lee@gmail.com>
2018-02-04 20:07:22 -0800

    Updating 064 to just update in place with the grid passed in.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode064.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode064.java
@@ -1,8 +1,5 @@
 package com.jlee.leetcodesolutions;
 
-import java.util.HashMap;
-import javafx.util.Pair;
-
 public class LeetCode064 {
   /*
    * Given a m x n grid filled with non-negative numbers, find a path from top
@@ -24,21 +21,20 @@ public class LeetCode064 {
     if(grid == null || grid.length < 1 || grid[0].length < 1)
       return 0;
     
-    HashMap<Pair<Integer,Integer>,Integer> map = new HashMap<>();
     for(int i = 0; i < grid.length; i++) {
       for(int j = 0; j < grid[i].length; j++) {
         if(i == 0 && j == 0)
-          map.put(new Pair(i,j), grid[i][j]);
+          grid[i][j] = grid[i][j];
         else if(i == 0)
-          map.put(new Pair(i,j), map.get(new Pair(i,j-1)) + grid[i][j]);
+          grid[i][j] = grid[i][j-1] + grid[i][j];
         else if(j == 0)
-          map.put(new Pair(i,j), map.get(new Pair(i-1,j)) + grid[i][j]);
+          grid[i][j] = grid[i-1][j] + grid[i][j];
         else {
-          int min = Math.min(map.get(new Pair(i-1,j)), map.get(new Pair(i,j-1)));
-          map.put(new Pair(i,j), min + grid[i][j]);
+          int min = Math.min(grid[i-1][j], grid[i][j-1]);
+          grid[i][j] = min + grid[i][j];
         }
       }
     }
-    return map.get(new Pair(grid.length-1,grid[0].length-1));
+    return grid[grid.length-1][grid[0].length-1];
   }
 }

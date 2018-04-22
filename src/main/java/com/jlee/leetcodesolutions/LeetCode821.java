2e80ba2c9a0b8a4a0334f3ce81691ec4cce719f1
John Lee <john.m.lee@gmail.com>
2018-04-21 19:24:27 -0700

    Adding problem description for 821.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode821.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode821.java
@@ -2,6 +2,20 @@ package com.jlee.leetcodesolutions;
 
 import java.util.TreeSet;
 
+/*
+ * Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+ * 
+ * Example 1:
+ * Input: S = "loveleetcode", C = 'e'
+ * Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ * 
+ * Note:
+ * 1. S string length is in [1, 10000].
+ * 2. C is a single character, and guaranteed to be in string S.
+ * 3. All letters in S and C are lowercase.
+ * 
+ * https://leetcode.com/problems/shortest-distance-to-a-character/
+ */
 public class LeetCode821 {
   public int[] shortestToChar(String S, char C) {
     // Store all char C positions. Using TreeSet to utilize the built in ceiling and

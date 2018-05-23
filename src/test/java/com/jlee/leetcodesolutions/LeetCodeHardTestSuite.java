c4b6e97ec6e39d19ae914410fa013a18c532e38d
John Lee <john.m.lee@gmail.com>
2018-05-23 02:06:28 -0700

    Adding solution for 839 - Similar String Groups.

---
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -0,0 +1,15 @@
+package com.jlee.leetcodesolutions;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ 
+  TestLeetCode839.class
+})
+  
+public class LeetCodeHardTestSuite {
+
+}

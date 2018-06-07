f82a7aeeba759f7de7d367b1d6e8820dd0dc40fd
John Lee <john.m.lee@gmail.com>
2018-06-06 20:22:54 -0700

    Adding solution for 786 - K-th Smallest Prime Fraction.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode786.class,
   TestLeetCode793.class,
   TestLeetCode798.class,
   TestLeetCode803.class,

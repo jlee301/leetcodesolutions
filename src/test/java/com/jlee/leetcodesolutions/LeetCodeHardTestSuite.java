f253571a7485de753bbf2923ba2eab7895425d85
John Lee <john.m.lee@gmail.com>
2018-06-13 01:44:44 -0700

    Adding solution for 773 - Sliding Puzzle.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode773.class,
   TestLeetCode778.class,
   TestLeetCode780.class,
   TestLeetCode782.class,

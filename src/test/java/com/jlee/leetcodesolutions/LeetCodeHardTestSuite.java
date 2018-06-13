e0b5fbbe00978c5e42edf599980d312504dd3fd2
John Lee <john.m.lee@gmail.com>
2018-06-12 20:27:34 -0700

    Adding solution for 778 - Swim in Rising Water.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode778.class,
   TestLeetCode780.class,
   TestLeetCode782.class,
   TestLeetCode786.class,
---
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode778.java
@@ -0,0 +1,34 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.LeetCode778;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLeetCode778 {
+  @Test
+  public void testProblemCase1() {
+    // Input: [[0,2],[1,3]]
+    // Output: 3
+    int[][] grid = {{0,2},{1,3}};
+    LeetCode778 solution = new LeetCode778();
+    Assert.assertEquals(3, solution.swimInWater(grid));
+  }
+
+  @Test
+  public void testProblemCase2() {
+    // Input: [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+    // Output: 16
+    int[][] grid = {{0,1,2,3,4},{24,23,22,21,5},{12,13,14,15,16},{11,17,18,19,20},{10,9,8,7,6}};
+    LeetCode778 solution = new LeetCode778();
+    Assert.assertEquals(16, solution.swimInWater(grid));
+  }
+
+  @Test
+  public void testAllZeroes() {
+    // Input: [[0,0],[0,0]]
+    // Output: 1
+    int[][] grid = {{0,0},{0,0}};
+    LeetCode778 solution = new LeetCode778();
+    Assert.assertEquals(0, solution.swimInWater(grid));
+  }
+}

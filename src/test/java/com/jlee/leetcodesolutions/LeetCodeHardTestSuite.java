a32c22e79685f1cdac47cf16bb6e1161db7cd4dd
John Lee <john.m.lee@gmail.com>
2018-06-18 20:51:22 -0700

    Adding solution for 753 - Cracking the Safe.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode753.class,
   TestLeetCode757.class,
   TestLeetCode761.class,
   TestLeetCode765.class,

40d16f7b16c4aaba5a482b43d58aaa51b4028a4d
John Lee <john.m.lee@gmail.com>
2018-06-20 03:29:10 -0700

    Adding solution for 745 - Prefix and Suffix Search.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode745.class,
   TestLeetCode749.class,
   TestLeetCode753.class,
   TestLeetCode757.class,

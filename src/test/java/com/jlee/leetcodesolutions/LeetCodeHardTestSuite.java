0704e8c1c0b987bb469f4164047b1f34b417b6de
John Lee <john.m.lee@gmail.com>
2018-06-22 01:30:01 -0700

    Adding solution for 736 - Parse Lisp Expression.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode736.class,
   TestLeetCode741.class,
   TestLeetCode745.class,
   TestLeetCode749.class,

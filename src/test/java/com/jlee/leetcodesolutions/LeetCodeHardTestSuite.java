a692ba361fff4b48f440eaed54152260c1bc97cd
John Lee <john.m.lee@gmail.com>
2018-07-18 19:18:44 -0700

    Adding solution for 639 - Decode Ways II.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode639.class,
   TestLeetCode664.class,
   TestLeetCode668.class,
   TestLeetCode675.class,

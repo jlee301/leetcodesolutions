5e4873f339e69892afafca3b55f1f9a5f5edabfd
John Lee <john.m.lee@gmail.com>
2018-07-18 20:04:19 -0700

    Adding solution for 632 - Smallest Range.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode632.class,
   TestLeetCode639.class,
   TestLeetCode664.class,
   TestLeetCode668.class,

76c01d8a2fcd938d50f9cc0a30bd14110918e8fd
John Lee <john.m.lee@gmail.com>
2018-07-23 22:03:50 -0700

    Adding solution for 629 - K Inverse Pairs Array.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode629.class,
   TestLeetCode630.class,
   TestLeetCode632.class,
   TestLeetCode639.class,

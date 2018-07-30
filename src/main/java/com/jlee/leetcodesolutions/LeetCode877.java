f12151d6d7a9409ea0bb145f9b766c77dec1dffd
John Lee <john.m.lee@gmail.com>
2018-07-29 18:55:01 -0700

    Adding comments for 877.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode877.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode877.java
@@ -20,6 +20,21 @@ public class LeetCode877 {
    * https://leetcode.com/contest/weekly-contest-95/problems/stone-game/
    */
   public boolean stoneGame(int[] piles) {
+    // 1. The number of piles is ALWAYS even
+    // 2. The sum of stones is odd
+    // 3. If you sum all odd piles or all even piles, one will be larger than the
+    // other
+    //
+    // Now if Alex goes first, Alex can force Lee to either pick all odds or all
+    // even piles (eg):
+    // 1. piles[0], piles[1], piles[2], piles[3]
+    // 2. Alex can choose piles[0] (even) or piles[3] (odd).  Alex chooses piles[0]
+    // 3. Lee can now choose piles[1] or piles[3] (only odds). 
+    // 4. If Lee picked piles[1], then Alex's choices are piles[2] or piles[3]
+    // 5. If Lee picked piles[3], then Alex's choices are piles[1] or piles[2]
+    //
+    // Either cases, Alex will be able to continue picking only even piles, still
+    // forcing Lee to pick only odd.  Alex will always win in this condition.
     return true;
   }  
 }

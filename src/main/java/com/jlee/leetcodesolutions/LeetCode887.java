9f24d8d59ee48fee33b003b708c0d05bdf42c8b4
John Lee <john.m.lee@gmail.com>
2018-08-04 20:06:26 -0700

    Code cleanup for 887.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode887.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode887.java
@@ -4,29 +4,27 @@ public class LeetCode887 {
   /*
    * https://leetcode.com/contest/weekly-contest-96/problems/projection-area-of-3d-shapes/
    */
-  public int projectionArea(int[][] grid) {
-    // Estimated xy area
-    int xy = grid.length * grid[0].length;
-    
+  public int projectionArea(int[][] grid) {    
     // find the max of each grid[i]
+    int xy = 0;
     int xz = 0;
-    int[] yz = new int[grid[0].length];    
+    int[] yzMax = new int[grid[0].length];    
     for(int[] g : grid) {
       int max = 0;
       for(int i = 0; i < g.length; i++) {
-        // If no cube was dropped (g[i] = 0), we must decrement from the xy estimated area
-        if(g[i] == 0)
-          xy--;
+        // If no cube was dropped (g[i] == 0), we must not count towards xy area
+        if(g[i] != 0)
+          xy++;
         max = Math.max(max, g[i]);
-        yz[i] = Math.max(yz[i], g[i]);
+        yzMax[i] = Math.max(yzMax[i], g[i]);
       }
       xz += max;
     }
     
-    int yzArea = 0;
-    for(int num : yz) 
-      yzArea += num;
+    int yz = 0;
+    for(int num : yzMax) 
+      yz += num;
     
-    return xy + xz + yzArea;
+    return xy + xz + yz;
   }
 }

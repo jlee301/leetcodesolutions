3d304cf219966b17316bfca1d8f3578a659b4310
John Lee <john.m.lee@gmail.com>
2018-08-20 23:40:20 -0700

    Adding solution for 466 - Count The Repetitions.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode466.class,
   TestLeetCode472.class,
   TestLeetCode480.class,
   TestLeetCode483.class,

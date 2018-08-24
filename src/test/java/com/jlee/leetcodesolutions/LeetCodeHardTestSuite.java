3805f384bdc718dc788be7cea9b9807f22458620
John Lee <john.m.lee@gmail.com>
2018-08-23 22:03:35 -0700

    Adding solution for 440 - K-th Smallest in Lexicographical Order.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode440.class,
   TestLeetCode446.class,
   TestLeetCode460.class,
   TestLeetCode466.class,

c78cefa93add758955b7903cea0603b981bb15dd
John Lee <john.m.lee@gmail.com>
2018-08-29 05:44:19 -0700

    Adding solution for 420 - Strong Password Checker.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode420.class,
   TestLeetCode432.class,
   TestLeetCode440.class,
   TestLeetCode446.class,

f2cddd6db65ac310b47abd184d38c6f88debc74e
John Lee <john.m.lee@gmail.com>
2018-09-04 04:12:05 -0700

    Adding solution for 391 - Perfect Rectangle.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode391.class,
   TestLeetCode403.class,
   TestLeetCode407.class,
   TestLeetCode410.class,

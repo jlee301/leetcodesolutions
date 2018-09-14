4cdd4eb1ef90e2644ef7cb9e7dc35f6b6799579d
John Lee <john.m.lee@gmail.com>
2018-09-13 19:10:04 -0700

    Adding solution for 330 - Patching Array.

---
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode330.java
@@ -0,0 +1,31 @@
+package com.jlee.leetcodesolutions;
+
+public class LeetCode330 {
+  /*
+   * Given a sorted positive integer array nums and an integer n, add/patch
+   * elements to the array such that any number in range [1, n] inclusive can be
+   * formed by the sum of some elements in the array. Return the minimum number of
+   * patches required.
+   * 
+   * https://leetcode.com/problems/patching-array/description/
+   */
+  public int minPatches(int[] nums, int n) {
+    int i = 0, patches = 0;
+    // Assume we start at [0,0], the min number (nextN) to patch to get to [0,1] is
+    // nextN = 1
+    int nextN = 1;
+    while(nextN <= n) {
+      if(i < nums.length && nums[i] <= nextN) {
+        nextN += nums[i];
+        i++;
+      }
+      else {
+        // If nums[i] is greater than the nextN, then we must patch nextN, creating
+        // [0, nextN) range
+        nextN += nextN;
+        patches++;
+      }
+    }
+    return patches;
+  }
+}
--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode330.class,
   TestLeetCode335.class,
   TestLeetCode336.class,
   TestLeetCode352.class,

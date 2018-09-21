71b05dcb7b20d610a047e3058085512ee077e330
John Lee <john.m.lee@gmail.com>
2018-09-21 00:21:03 -0700

    Adding data driven version of the tests for 316.

--- src/test/java/com/jlee/leetcodesolutions/TestLeetCode316.java
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode316.java
@@ -1,6 +1,13 @@
 package com.jlee.leetcodesolutions;
 
 import com.jlee.leetcodesolutions.LeetCode316;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,5 +38,26 @@ public class TestLeetCode316 {
     String s = "eywdgenmcnzhztolafcfnirfpuxmfcenlppegrcalgxjlajxmphwidqqtrqnmmbssotoywfrtylm";
     LeetCode316 solution = new LeetCode316();
     Assert.assertEquals("chzafipuegjlxdqnbsotwrym", solution.removeDuplicateLetters(s));
+  }
+  
+  @Test
+  public void testProblemCase5() {
+    String fileName = "src/test/java/com/jlee/leetcodesolutions/TestLeetCode316.txt";
+    List<String> list = new ArrayList<>();
+    try {
+      Stream<String> stream = Files.lines(Paths.get(fileName));
+      list = stream.collect(Collectors.toList());
+      stream.close();
+    }
+    catch(IOException e) {
+      Assert.fail(e.toString());
+    }
+    
+    LeetCode316 solution = new LeetCode316();
+    for(String line : list) {
+      String[] sdata = line.split(",");
+      // System.out.println(sdata[0] + " | " + sdata[1]);
+      Assert.assertEquals(sdata[1], solution.removeDuplicateLetters(sdata[0]));
+    }
   }
 }

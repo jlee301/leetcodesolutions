d2105a6132c8cb14d26c04360de57ba21ccf6ab9
John Lee <john.m.lee@gmail.com>
2018-09-25 15:47:11 -0700

    Adding solution for 297 - Serialize and Deserialize Binary Tree.

---
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode297.java
@@ -0,0 +1,53 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.TreeNode;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeetCode297 {
+  /*
+   * Serialization is the process of converting a data structure or object into a
+   * sequence of bits so that it can be stored in a file or memory buffer, or
+   * transmitted across a network connection link to be reconstructed later in the
+   * same or another computer environment.
+   * 
+   * Design an algorithm to serialize and deserialize a binary tree. There is no
+   * restriction on how your serialization/deserialization algorithm should work.
+   * You just need to ensure that a binary tree can be serialized to a string and
+   * this string can be deserialized to the original tree structure.
+   * 
+   * https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+   */
+  //Encodes a tree to a single string.
+  public String serialize(TreeNode root) {
+    if(root == null)
+      return "#|";
+    
+    String result = root.val + "|";
+    result = result + serialize(root.left) + serialize(root.right);
+    return result;
+  }
+
+  // Decodes your encoded data to tree.
+  public TreeNode deserialize(String data) {
+    // 1|2|#|#|3|4|#|#|5|#|#|
+    // [1,2,#,#,3,4,#,#,5,#,#]
+    String[] sdata = data.split("\\|");
+    Queue<String> queue = new LinkedList<>(Arrays.asList(sdata));
+    return helper(queue);
+  }
+  
+  private TreeNode helper(Queue<String> queue) {
+    if(queue.peek().equals("#")) {
+      queue.poll();
+      return null;
+    }
+    
+    TreeNode node = new TreeNode(Integer.valueOf(queue.poll()));
+    node.left = helper(queue);
+    node.right = helper(queue);
+    
+    return node;
+  }
+}
---
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode297.java
@@ -0,0 +1,38 @@
+package com.jlee.leetcodesolutions;
+
+import com.jlee.leetcodesolutions.LeetCode297;
+import com.jlee.leetcodesolutions.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLeetCode297 {
+  @Test
+  public void testProblemCase1() {
+    TreeNode root = new TreeNode(1);
+
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    
+    root.right.left = new TreeNode(4);
+    root.right.right = new TreeNode(5);
+    
+    LeetCode297 solution = new LeetCode297();
+    String result = solution.serialize(root);
+    // System.out.println(result);
+    TreeNode node = solution.deserialize(result);
+    Assert.assertEquals(1, node.val);
+    
+    Assert.assertEquals(2, node.left.val);
+    Assert.assertEquals(3, root.right.val);
+    
+    Assert.assertNull(node.left.left);
+    Assert.assertNull(node.left.right);
+    Assert.assertEquals(4, node.right.left.val);
+    Assert.assertEquals(5, node.right.right.val);
+    
+    Assert.assertNull(node.right.left.left);
+    Assert.assertNull(node.right.left.right);
+    Assert.assertNull(node.right.right.left);
+    Assert.assertNull(node.right.right.right);
+  }
+}

5d9eeed30df87cb7f3e758e2962877c165033f96
John Lee <john.m.lee@gmail.com>
2018-09-26 18:42:29 -0700

    Adding solution for 282 - Expression Add Operators.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode282.class,
   TestLeetCode295.class,
   TestLeetCode297.class,
   TestLeetCode301.class,

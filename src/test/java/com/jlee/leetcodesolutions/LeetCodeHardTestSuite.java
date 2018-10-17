e33a8161f082bad7d4b6bdcf3005647de2f569df
John Lee <john.m.lee@gmail.com>
2018-10-16 18:21:05 -0700

    Adding solution for 188 - Best Time to Buy and Sell Stock IV.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode188.class,
   TestLeetCode212.class,
   TestLeetCode214.class,
   TestLeetCode218.class,

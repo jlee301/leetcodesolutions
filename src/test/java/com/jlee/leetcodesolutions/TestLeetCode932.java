63c1fb7a431c8439a877c6e3a0ce1b20243b1a1f
John Lee <john.m.lee@gmail.com>
2018-10-28 01:42:16 -0700

    Updating test cases for 932 to use helper function.

--- src/test/java/com/jlee/leetcodesolutions/TestLeetCode932.java
+++ src/test/java/com/jlee/leetcodesolutions/TestLeetCode932.java
@@ -12,23 +12,7 @@ public class TestLeetCode932 {
     int N = 4;
     LeetCode932 solution = new LeetCode932();
     int[] result = solution.beautifulArray(N);
-    
-    // Checking to see 1 thru N are in the array
-    List<Integer> list = new ArrayList<>();
-    for(int n : result)
-      list.add(n);
-    
-    for(Integer i = 1; i <= N; i++)
-      Assert.assertTrue(list.contains(i));
-    
-    // i < k < j, no k exist where 2*A[k] != A[i] + A[j]
-    for(int i = 0; i < result.length-2; i++) {
-      for(int j = i + 2; j < result.length-1; j++) {
-        int IJSum = result[i] + result[j];
-        for(int k = i + 1; k < j; k++)
-          Assert.assertTrue(2*result[k] != IJSum);
-      }
-    }
+    validateBeautifulArray(result, N);
   }
 
   @Test
@@ -36,7 +20,10 @@ public class TestLeetCode932 {
     int N = 5;
     LeetCode932 solution = new LeetCode932();
     int[] result = solution.beautifulArray(N);
-    
+    validateBeautifulArray(result, N);
+  }
+  
+  private void validateBeautifulArray(int[] result, int N) {
     // Checking to see 1 thru N are in the array
     List<Integer> list = new ArrayList<>();
     for(int n : result)

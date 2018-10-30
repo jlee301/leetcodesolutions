27260511a64f2b29961d6793cfbc14d933b92344
John Lee <john.m.lee@gmail.com>
2018-10-29 21:05:18 -0700

    Adding solution for 132 - Palindrome Partitioning II.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode132.class,
   TestLeetCode135.class,
   TestLeetCode140.class,
   TestLeetCode145.class,

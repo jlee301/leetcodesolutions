8c9399df0241b487c683326d950c1150d8c681af
John Lee <john.m.lee@gmail.com>
2018-11-01 17:43:11 -0700

    Adding solution for 124 - Binary Tree Maximum Path Sum.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode124.class,
   TestLeetCode126.class,
   TestLeetCode128.class,
   TestLeetCode132.class,

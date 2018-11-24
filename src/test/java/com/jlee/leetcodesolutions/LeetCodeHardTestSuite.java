485c351760a61e0e7832d01ed3288a07e8b04edb
John Lee <john.m.lee@gmail.com>
2018-11-24 15:46:13 -0800

    Adding solution for 004 - Median of Two Sorted Arrays.

--- src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
+++ src/test/java/com/jlee/leetcodesolutions/LeetCodeHardTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({ 
+  TestLeetCode004.class,
   TestLeetCode010.class,
   TestLeetCode023.class,
   TestLeetCode025.class,

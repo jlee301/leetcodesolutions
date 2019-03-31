032d697b4aea81579ffdf3dcd648f6e1d025b701
John Lee <john.m.lee@gmail.com>
2019-03-31 03:24:24 -0700

    Updating 1031 to not use extra memory and change the dfs function to return the count.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode1031.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode1031.java
@@ -1,49 +1,47 @@
 package com.jlee.leetcodesolutions;
 
-import java.util.HashSet;
-
 public class LeetCode1031 {
   /*
    * https://leetcode.com/problems/number-of-enclaves/
    */
   private boolean canReachBorder;
-  private boolean[][] visited;
   private int[][] moves = { {-1,0}, {1,0}, {0,-1}, {0,1} };
   
   public int numEnclaves(int[][] A) {
     int count = 0;
-    visited = new boolean[A.length][A[0].length];
     // Traverse every element of A to look for a land spot
     for(int i = 0; i < A.length; i++) {
       for(int j = 0; j < A[i].length; j++) {
         canReachBorder = false;
-        HashSet<String> set = new HashSet<>();
-        if(!visited[i][j] && A[i][j] == 1)
-          dfs(A, i, j, set);
-        
-        count += canReachBorder ? 0 : set.size(); 
+        int currCount = 0;
+        if(A[i][j] == 1)
+          currCount = dfs(A, i, j);
+          
+        count += canReachBorder ? 0 : currCount;        
       }
     }    
     return count;
   }
   
-  private void dfs(int[][] A, int i, int j, HashSet<String> set) {
+  private int dfs(int[][] A, int i, int j) {
     if(i < 0 || i == A.length || j < 0 || j == A[0].length) {
       // If you reach the border, then all of the connected land will not count
       canReachBorder = true;
-      return;
+      return 0;
     }
     
-    if(A[i][j] == 0 || visited[i][j])
-      return;
+    if(A[i][j] < 1)
+      return 0;
     
-    set.add(i + "_" + j);
-    visited[i][j] = true;
+    // A[i][j] == -1 == visited
+    A[i][j] = -1;
     // Check four-directional adjacent nodes
+    int count = 1;
     for(int[] move : moves) {
       int nextI = i + move[0];
       int nextJ = j + move[1];
-      dfs(A, nextI, nextJ, set);
+      count += dfs(A, nextI, nextJ);
     }
+    return count;
   }
 }

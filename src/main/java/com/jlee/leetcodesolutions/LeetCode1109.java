82f820f0deaae7b3b8e88cf94a26e9f4622571c2
John Lee <john.m.lee@gmail.com>
2019-07-10 17:59:25 -0700

    Updating 1109 to be O(n) time complexity.

--- src/main/java/com/jlee/leetcodesolutions/LeetCode1109.java
+++ src/main/java/com/jlee/leetcodesolutions/LeetCode1109.java
@@ -6,10 +6,16 @@ public class LeetCode1109 {
    */
   public int[] corpFlightBookings(int[][] bookings, int n) {
     int[] result = new int[n];
+
     for(int[] booking : bookings) {
-      for(int i = booking[0] - 1; i < booking[1]; i++)
-        result[i] += booking[2];
+      result[booking[0] - 1] += booking[2];
+      if(booking[1] < n)
+        result[booking[1]] -= booking[2];
     }
+    
+    for(int i = 1; i < result.length; i++)
+      result[i] += result[i-1];
+    
     return result;
   }
 }
